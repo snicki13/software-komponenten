c6166e9ac55196b80c813d2f6c90bfbdc6aa553d
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2021-02-24 01:47:29 +0100

    Initial commit

---
+++ src/main/scala/blatt1/Main.scala
@@ -0,0 +1,6 @@
+@main def hello: Unit = {
+    println("Hello Blatt 1!")
+    println(msg)
+}
+
+def msg = "I was compiled by Scala 3. :)"
---
+++ src/main/scala/blatt2/Main.scala
@@ -0,0 +1,8 @@
+package blatt2
+
+@main def hello: Unit = {
+    println("Hello Blatt 2!")
+    println(msg)
+}
+
+def msg = "I was compiled by Scala 3. :)"
